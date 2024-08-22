04e62a81f5c3ae9464d9d789d89a0c73ff9f62a0
Kristina Gurachenkova <gurachenkova@yandex.ru>
2024-08-22 17:47:53 +0500

    added style css slider

--- src/components/TerrainSlider/TerrainSlider.jsx
+++ src/components/TerrainSlider/TerrainSlider.jsx
@@ -2,8 +2,8 @@ import image1 from "../../images/terrain.png";
 import image2 from "../../images/pirs1.jpg";
 import image3 from "../../images/pirs2.jpg";
 import image4 from "../../images/pirs3.jpg";
-import React, { useState, useRef, useEffect } from 'react';
-import Slider from 'react-slick';
+import React, { useState, useRef, useEffect } from "react";
+import Slider from "react-slick";
 
 export default function TerrainSlider() {
   const terrainImages = [image1, image2, image3, image4];
@@ -40,47 +40,58 @@ export default function TerrainSlider() {
   };
 
   useEffect(() => {
-    window.addEventListener('resize', updateSlider);
+    window.addEventListener("resize", updateSlider);
     return () => {
-      window.removeEventListener('resize', updateSlider);
+      window.removeEventListener("resize", updateSlider);
     };
   }, []);
 
-  return(
+  return (
     <section className="terrain" id="terrain">
-    <h2 className="section-header">Местность</h2>
-    <div className="terrain__flex-container">
-    <div className="terrain__slider-container" style={{ position: 'relative' }}>
-    <Slider ref={sliderRef} {...settings}>
-        {terrainImages.map((image, index) => (
-            <img key={index} className="terrain__image" src={image} alt="фото пирсов" />
-        ))}
-      </Slider>
+      <h2 className="section-header">Местность</h2>
+      <div className="terrain__flex-container">
+        <div className="terrain__slider-container">
+          <Slider ref={sliderRef} {...settings}>
+            {terrainImages.map((image, index) => (
+              <img
+                key={index}
+                className="terrain__image"
+                src={image}
+                alt="фото пирсов"
+              />
+            ))}
+          </Slider>
 
-      <div className="custom-dots">
-        {terrainImages.map((_, index) => (
-          <span
-            key={index}
-            className={`custom-dot ${currentSlide === index ? 'active' : ''}`}
-            onClick={() => goToSlide(index)}
-          ></span>
-        ))}
-      </div>
-      <div className="slider-navigation">
-        <button className="nav-button left" onClick={prevSlide}>◀</button>
-        <button className="nav-button right" onClick={nextSlide}>▶</button>
-      </div>
-      </div>
+          <div className="terrain__slider-dots">
+            {terrainImages.map((_, index) => (
+              <span
+                key={index}
+                className={`terrain__slider-dot ${
+                  currentSlide === index ? "active" : ""
+                }`}
+                onClick={() => goToSlide(index)}
+              ></span>
+            ))}
+          </div>
+        </div>
 
-      <div className="terrain__description">
-        <h3 className="terrain__header">Комфортабельные места подхода и пирсы</h3>
-        <p className="terrain__text">
-          Также как консультация с широким активом требует от нас анализа инновационных методов управления процессами.
-          В своём стремлении улучшить пользовательский опыт мы упускаем, что активно развивающиеся страны третьего мира
-          объединены в целые кластеры себе подобных.
-        </p>
+        <div className="terrain__description">
+          <h3 className="terrain__header">
+            Комфортабельные места подхода и пирсы
+          </h3>
+          <p className="terrain__text">
+            Также как консультация с широким активом требует от нас анализа
+            инновационных методов управления процессами. В своём стремлении
+            улучшить пользовательский опыт мы упускаем, что активно
+            развивающиеся страны третьего мира объединены в целые кластеры себе
+            подобных.
+          </p>
+          <div className="terrain__slider-navigation">
+            <button className="terrain__nav-button terrain__nav-button_left" onClick={prevSlide}></button>
+            <button className="terrain__nav-button terrain__nav-button_right" onClick={nextSlide}></button>
+          </div>
+        </div>
       </div>
-    </div>
-  </section>
-  )
+    </section>
+  );
 }
