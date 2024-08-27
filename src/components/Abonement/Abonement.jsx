b57b87d2965d13939f65feff09b3dc2256524a67
Kristina Gurachenkova <gurachenkova@yandex.ru>
2024-08-27 15:01:20 +0500

    fixed bug, added component rules

--- src/components/Abonement/Abonement.jsx
+++ src/components/Abonement/Abonement.jsx
@@ -1,7 +1,30 @@
+import abonements from "../../utils/abonements";
+
 export default function Abonement() {
   return (
-    <div>
-      
+    <div className="abonement-container">
+      {abonements.map((x, index) => (
+        <AbonementCard
+          key={index}
+          image={x.image}
+          description={x.description}
+          text={x.text}
+          price={x.price}
+        />
+      ))}
     </div>
-  )
+  );
+}
+
+function AbonementCard({ image, description, text, price }) {
+  return (
+    <div className="abonement-card">
+      <img src={image} alt="изображение абонемента" />
+      <p className="abonement-card__description">{description}</p>
+      <div className="abonement-card__text-container">
+        <p className="abonement-card__text">{text}</p>
+        <p className="abonement-card__text">{price}</p>
+      </div>
+    </div>
+  );
 }
---
+++ src/utils/abonements.js
@@ -0,0 +1,35 @@
+import abonement1 from "../images/abonement1.svg";
+import abonement2 from "../images/abonement2.svg";
+
+const abonements = [
+  {
+    image: abonement1,
+    description: (
+      <>
+        <p className="abonement__description">Клубная карта дает право на неограниченное посещение рыболовного клуба в период:
+          с апреля по декабрь 2023 года включительно. Самовылов не более
+          <strong style={{ color: 'blue' }}> 5 кг/сутки</strong>,
+          с ограничением на <strong style={{ color: 'blue' }}>4 удочки</strong>.</p>
+      </>
+    ),
+    text: "Стоимость:",
+    price: "16 000 ₽",
+  },
+
+  {
+    image: abonement2,
+    description: (
+      <>
+        <p className="abonement__description">Клубная карта дает право на неограниченное посещение рыболовного клуба в период:
+          с апреля по декабрь 2023 года включительно. Самовылов не более
+          <strong style={{ color: 'orange' }}> 3 кг/сутки</strong>,
+          с ограничением на <strong style={{ color: 'orange' }}>4 удочки</strong>.</p>
+      </>
+    ),
+    text: "Стоимость:",
+    price: "12 000 ₽",
+  }
+
+]
+
+export default abonements;
